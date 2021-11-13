34500b49d8dfee6df06df852604c05746a0930ed
IH-MethodBox <65013694+lzr2006@users.noreply.github.com>
2021-11-13 18:30:30 +0800

    Add files via upload

---
+++ ASPCORE/JianshuMicroTools/Tools/UserAssetsViewer.aspx
@@ -0,0 +1,66 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="UserAssetsViewer.aspx.cs" Inherits="Tools_UserAssetsViewer" %>
+
+<%@ Register assembly="System.Web.DataVisualization, Version=4.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35" namespace="System.Web.UI.DataVisualization.Charting" tagprefix="asp" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+        <script language="javascript" type="text/javascript">
+        function getEmployeecode(empid)
+        {
+            alert("员工编号："+empid);
+        }
+    </script>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        	<h1 id="用户资产查询工具" style="box-sizing: border-box; margin: 24px 0px 16px; font-weight: 600; line-height: 1.25; font-size: 2em; padding-bottom: 0.3em; border-bottom: 1px solid rgb(234, 236, 239); color: rgb(36, 41, 46); font-family: -apple-system, BlinkMacSystemFont, &quot;Segoe UI&quot;, Helvetica, Arial, sans-serif, &quot;Apple Color Emoji&quot;, &quot;Segoe UI Emoji&quot;; font-style: normal; font-variant-ligatures: normal; font-variant-caps: normal; letter-spacing: normal; orphans: 2; text-align: left; text-indent: 0px; text-transform: none; white-space: normal; widows: 2; word-spacing: 0px; -webkit-text-stroke-width: 0px; background-color: rgb(255, 255, 255); text-decoration-thickness: initial; text-decoration-style: initial; text-decoration-color: initial;">用户资产查询工具</h1>
+        </div>
+		<p>
+			输入用户的Url：</p>
+		<asp:TextBox ID="TextBox1" runat="server" Font-Names="Agency FB" Font-Size="Large" Height="21px" Width="366px"></asp:TextBox>
+    	<asp:Button ID="Button1" runat="server" Height="27px" OnClick="Button1_Click" Text="确认" Width="83px" />
+
+
+        <p>
+            &nbsp;</p>
+
+    <h2>
+        该用户的资产信息</h2>
+
+
+        <asp:Label ID="Label_FP" runat="server" Text="他的简书钻："></asp:Label>
+        <p>
+            <asp:Label ID="Label_FTN" runat="server" Text="他的简书贝："></asp:Label>
+        </p>
+        <asp:Chart ID="Chart_Info" runat="server" Width="311px">
+          <%--  <borderskin skinstyle="Emboss"></borderskin>--%>
+            <Series>
+                <asp:Series Name="Series1"  ChartType="Pie" MarkerSize="8" MarkerStyle="Circle">
+                </asp:Series>
+            </Series>
+            <ChartAreas>
+                <asp:ChartArea Name="ChartArea1" >
+                </asp:ChartArea>
+            </ChartAreas>
+        </asp:Chart>
+
+        <asp:Chart ID="Chart1" runat="server">
+            <series>
+                <asp:Series Name="Series1">
+                </asp:Series>
+            </series>
+            <chartareas>
+                <asp:ChartArea Name="ChartArea1">
+                </asp:ChartArea>
+            </chartareas>
+        </asp:Chart>
+    </form>
+
+    
+</body>
+</html>
---
+++ ASPCORE/JianshuMicroTools/Tools/UserAssetsViewer.aspx.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Script.Serialization;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Tools_UserAssetsViewer : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        //获取用户的简书钻
+        string Geter = BasicApis.GetUserJsonDataApi(TextBox1.Text);
+        JavaScriptSerializer aparser = new JavaScriptSerializer();
+        Struct.FP nowUserFp = aparser.Deserialize<Struct.FP>(Geter);
+        nowUserFp.jsd_balance = nowUserFp.jsd_balance / 1000;
+        //现在获取用户的总资产
+        //是的 我又要开始传统艺能了
+        string htmlObject = BasicApis.GetUserPCHtmlDataApi(TextBox1.Text);
+        int userAssets = Convert.ToInt32(
+            htmlObject.Split(new string[] { "<div>总资产</div>" }
+        , StringSplitOptions.None)[0].Split(new string[] 
+        { "<div class=\"meta-block\">" },StringSplitOptions.None)[6].Split(new string[] 
+            { "<p>" },StringSplitOptions.None)[1].Split(new string[]{ "</p>" },
+            StringSplitOptions.None)[0]
+            );
+        int nowUserFTN = userAssets - nowUserFp.jsd_balance;
+
+        this.Label_FP.Text = $"他的简书钻：{nowUserFp.jsd_balance}";
+        this.Label_FTN.Text = $"他的简书贝：{nowUserFTN}";
+
+        //创建数据显示
+        var dt = CreatData(nowUserFp.jsd_balance,nowUserFTN);
+
+        Chart_Info.DataSource = dt;//绑定数据
+        Chart_Info.Series["Series1"].ChartType = System.Web.UI.DataVisualization.Charting.SeriesChartType.Pie;//设置图表类型
+        Chart_Info.Series[0].XValueMember = "AssetsItem";//X轴数据成员列
+        Chart_Info.Series[0].YValueMembers = "Count";//Y轴数据成员列
+        Chart_Info.Series[0].LegendText = "AssetsItem";
+        Chart_Info.Series[0].IsValueShownAsLabel = true;//显示坐标值
+
+        Chart1.DataSource = dt;//绑定数据
+        Chart1.Series["Series1"].ChartType = System.Web.UI.DataVisualization.Charting.SeriesChartType.SplineArea;//设置图表类型
+        Chart1.Series[0].XValueMember = "AssetsItem";//X轴数据成员列
+        Chart1.Series[0].YValueMembers = "Count";//Y轴数据成员列
+        Chart1.Series[0].LegendText = "AssetsItem";
+        Chart1.Series[0].IsValueShownAsLabel = true;//显示坐标值
+    }
+
+    /// <summary>
+    /// 创建一张二维数据表
+    /// </summary>
+    /// <returns>Datatable类型的数据表</returns>
+    public DataTable CreatData(int FP,int FTN)
+    {
+        DataTable dt = new DataTable();
+        dt.Columns.Add("AssetsItem", System.Type.GetType("System.String"));
+        dt.Columns.Add("Count", System.Type.GetType("System.String"));
+
+        string[] n = new string[] { "简书钻","简书贝"};
+        string[] c = new string[] { FP.ToString(),FTN.ToString()};
+
+        DataRow dr = dt.NewRow();
+        dr["AssetsItem"] = n[0];
+        dr["Count"] = c[0];
+        dt.Rows.Add(dr);
+
+        DataRow dra = dt.NewRow();
+        dra["AssetsItem"] = n[1];
+        dra["Count"] = c[1];
+
+        dt.Rows.Add(dra);
+
+        return dt;
+    }
+}
